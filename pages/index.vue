e97684d79b3a52a8796d5f74ef2d7b263eeb0af9
Yelt-dev <yeltsin.lopez94@gmail.com>
2022-11-09 20:39:03 -0600

    commit inicial de proyecto

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Registrarse/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Registrarse from '~/components/Registrarse.vue';
+
+export default Vue.extend({
+    name: "IndexPage",
+    components: { Registrarse }
+})
+</script>
